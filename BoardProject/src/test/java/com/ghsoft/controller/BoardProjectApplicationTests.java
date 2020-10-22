c1e525ce492dd9419fe0ffd17a99ae6537e358cf
Geonhee Cho <gunny91@iastate.edu>
2020-10-22 11:52:09 +0900

    Real BootStrap practice to make board

---
+++ BoardProject/src/test/java/com/ghsoft/controller/BoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghsoft.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
