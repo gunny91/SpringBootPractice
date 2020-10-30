6908ca0fab43ad303519b59d1280f39b7c14fdde
Geonhee Cho <gunny91@iastate.edu>
2020-10-30 16:59:27 +0900

    spring boot update

--- SpringBootVer6/src/main/java/com/ghsoft/persistence/WebBoardRepository.java
+++ SpringBootVer6/src/main/java/com/ghsoft/persistence/WebBoardRepository.java
@@ -1,10 +1,51 @@
 package com.ghsoft.persistence;
 
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
+import com.ghsoft.domain.QWebBoard;
 import com.ghsoft.domain.WebBoard;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
 
-public interface WebBoardRepository extends CrudRepository<WebBoard, Long> {
+// Querydsl을 사용하기 위해서 Querydsl을 상속받는다.
+public interface WebBoardRepository 
+	extends CrudRepository<WebBoard, Long>, QuerydslPredicateExecutor<WebBoard> {
+
+	public default Predicate makePredicate(String type, String string) {
+		BooleanBuilder builder = new BooleanBuilder();
+		QWebBoard board = QWebBoard.webBoard;
+
+		builder.and(board.bno.gt(0)); // board.bno > 0
+		
+		if(type==null) {return builder;}
+		switch(type) {
+		case "t" : 
+			builder.and(board.title.like("%"+string+"%"));
+			break;
+		case "c":
+			builder.and(board.title.like("%"+string+"%"));
+			break;
+		case "w":
+			builder.and(board.title.like("%"+string+"%"));
+			break;
+		}
+		return builder;
+	}
+	
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- SpringBootVer6/src/test/java/com/ghsoft/SpringBootVer6ApplicationTests.java
+++ SpringBootVer6/src/test/java/com/ghsoft/SpringBootVer6ApplicationTests.java
@@ -6,6 +6,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -27,14 +31,35 @@ public class SpringBootVer6ApplicationTests {
 	/**
 	 * Make the 300 data list
 	 */
+//	@Test
+//	public void insertBoard() {
+//		IntStream.range(0, 300).forEach(i ->{WebBoard board = new WebBoard();
+//		
+//		board.setTitle("Temp"+i);
+//		board.setContext("Content temp"+i);
+//		board.setWriter("user0"+(i%10));
+//		repo.save(board);
+//	});
+//	}
+	
+//	@Test
+//	public void listTest1() {
+//		Pageable pageable = PageRequest.of(0, 20, Direction.DESC, "bno");
+//		Page<WebBoard> result = repo.findAll(repo.makePredicate(null, null), pageable);
+//		log.info("PAGE : " + result.getPageable());
+//		log.info("----------------------------------------------------------");
+//		result.getContent().forEach(board -> log.info("" + board));
+//		
+//	}
+	
 	@Test
-	public void insertBoard() {
-		IntStream.range(0, 300).forEach(i ->{WebBoard board = new WebBoard();
+	public void listTest2() {
+		Pageable pageable = PageRequest.of(0, 2, Direction.DESC, "bno");
+		Page<WebBoard> result = repo.findAll(repo.makePredicate("t", "1"), pageable);
+		log.info("PAGE : " + result.getPageable());
+		log.info("----------------------------------------------------------");
+		result.getContent().forEach(board -> log.info("" + board));
 		
-		board.setTitle("Temp"+i);
-		board.setContext("Content temp"+i);
-		board.setWriter("user0"+(i%10));
-		repo.save(board);
-	});
 	}
+	
 }
